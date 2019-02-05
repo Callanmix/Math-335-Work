f98bfa5706286de79385ea52adf2f2a340e47b19
callanmix <callanmix@gmail.com>
2019-02-04 23:42:24 -0700

    task 9 first push

--- Case_Study_04/analysis/gun_practice.R
+++ Case_Study_04/analysis/gun_practice.R
@@ -3,24 +3,36 @@ gun_data <- read_csv(here::here("/Data/full_data_guns.csv"))
 library(dplyr)
 
 new_data_gun %>%
-  count(intent) %>%  count(education) %>%  count(race)
-  
+  count(intent, race) %>% 
+  ggplot(aes(x = race, y = n), na.rm = TRUE) +
+  geom_point(aes(color = n), show.legend = FALSE) +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  facet_wrap(facets = vars(intent), nrow = 1) +
+  labs(title = 'Deaths per Intent per Race', y = 'Number of Deaths', x = 'Race')
 
 
 
   
 new_data_gun <- gun_data %>% 
   select(year:intent, sex:race, place, education) %>% 
-  mutate(Season = case_when(month %in% c('01','02','12')~'winter',
-                            month %in% c('03','04','05')~'spring',
-                            month %in% c('06','07','08')~'summer',
-                            TRUE ~ 'fall'))
-
+  mutate(Season = case_when(month %in% c('01','02','12')~'Winter',
+                            month %in% c('03','04','05')~'Spring',
+                            month %in% c('06','07','08')~'Summer',
+                            TRUE ~ 'Fall'))
+new_data_gun %>% 
+  count(Season, sex, race) %>% 
+  ggplot(aes(x = race, y = n)) +
+  geom_point(aes(color = sex)) +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  facet_wrap(facets = vars(Season), nrow = 1) +
+  labs(title = "Deaths per Race Every Season", y = "Nubmer of Deaths", color = 'Gender',
+       x = 'Race')
 
 new_data_gun %>% 
-  select(year, count(intent, na.rm = TRUE)) %>% 
-  ggplot(aes(x = intent, y = n)) +
-  geom_histogram()
-  facet_wrap(facets = vars(education), nrow = 1)
-
-
+  count(education, Season) %>% 
+  ggplot(aes(x = education, y = n)) +
+  geom_point(aes(color = Season)) +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  facet_wrap(facets = vars(Season), nrow = 1) +
+  labs(title = "Deaths by Education Every Season", y = "Nubmer of Deaths", color = 'Season',
+       x = 'Race')
