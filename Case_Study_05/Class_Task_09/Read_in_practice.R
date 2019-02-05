de60fb75d908726a0f144e47819fd4b523931bcb
callanmix <callanmix@gmail.com>
2019-02-05 12:08:59 -0700

    made on image with 3 graphs

--- Case_Study_05/Class_Task_09/Read_in_practice.R
+++ Case_Study_05/Class_Task_09/Read_in_practice.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(downloader)
+library(gridExtra)
+library(ggplot2)
 download(mode = "wb")
 dart_csv <- read_csv("https://github.com/byuistats/data/raw/master/Dart_Expert_Dow_6month_anova/Dart_Expert_Dow_6month_anova.csv")
 dart_dta <- read_dta("https://github.com/byuistats/data/raw/master/Dart_Expert_Dow_6month_anova/Dart_Expert_Dow_6month_anova.dta")
@@ -7,16 +9,19 @@ dart_sav <- read_sav("https://github.com/byuistats/data/raw/master/Dart_Expert_D
 dart_rds <- read_rds(gzcon(url("https://github.com/byuistats/data/raw/master/Dart_Expert_Dow_6month_anova/Dart_Expert_Dow_6month_anova.RDS")))
 dart_xlsx <- download(mode = "wb", url = "https://github.com/byuistats/data/raw/master/Dart_Expert_Dow_6month_anova/Dart_Expert_Dow_6month_anova.xlsx")
 
-dart_csv %>% 
+
+p1 <- dart_csv %>% 
   ggplot(aes(x = variable, y = value)) +
   geom_jitter(aes(color = variable))
 
-
-dart_csv %>% 
+p2 <- dart_csv %>% 
   ggplot(aes(x = variable, y = value)) +
-  geom_boxplot()
-  
-dart_csv %>% 
+  geom_boxplot() 
+
+p3 <- dart_csv %>%   
   group_by(variable) %>% summarise(ave = mean(value)) %>% 
   ggplot(aes(y = ave, x = variable)) +
   geom_point()
+
+grid.arrange(p1,p2,p3)  
+
