9426c8acaedd063b6e3f8be3fb49590866bdffb8
callanmix <callanmix@gmail.com>
2019-02-28 11:37:56 -0700

    some changes

--- Case_Study_07/Class_Task_14/task_14.R
+++ Case_Study_07/Class_Task_14/task_14.R
@@ -1,21 +1,18 @@
 library(tidyverse)
 library(stringi)
-script <- read_csv(here::here("Data/lds-scripture.csv"))
+scriptures <- read_csv(here::here("Data/lds-scripture.csv"))
 
 
-script %>% 
-  filter($volume_lds_url == "ot") %>% 
-  stri_stats_latex(scripture_text)
 
 
-stri_stats_latex(filter(script$volume_lds_url == "ot"))
-
-str_locate_all(script$scripture_text, pattern = "\\w")
-
-script %>% 
-  filter(volume_lds_url == "ot") %>% 
-  group_by(scripture_text) %>% 
-  str_count(pattern = "\\b")
+scriptures %>% 
+  group_by(verse_short_title, volume_title) %>% 
+  summarise(number = stri_stats_latex(scripture_text)["Words"]) %>% 
+  ungroup() %>% 
+  group_by(volume_title) %>% 
+  summarize(mean = mean(number)) %>%  
+  ggplot(aes(y = volume_title, x = mean)) +
+  geom_point()
 
 
 
@@ -23,10 +20,35 @@ script %>%
 
 
 
+scriptures %>% 
+  filter(volume_title %in% c("New Testament", "Book of Mormon")) %>% 
+  group_by(volume_title) %>% 
+  summarise(text = str_c(scripture_text, collapse = " "),
+            count = str_count(text, "Jesus")) %>%
+  select(volume_title, count) %>% 
+  view()
 
 
-str_count("cody and elena", "\\w")
 
-stringi::stri_stats_latex("cody and elena")[4]
+
+
+
+  
+  
+flevels <- scriptures %>% filter(volume_title == "Book of Mormon") %>% .$book_title %>% unique()
+
+scriptures %>% 
+  filter(volume_title == "Book of Mormon") %>% 
+  group_by(book_title, verse_id) %>% 
+  mutate(number = stri_stats_latex(scripture_text)["Words"]) %>% 
+  ungroup() %>% 
+  ggplot(aes(x = factor(book_title, levels = flevels), y = number)) +
+  geom_jitter(width = .25, height = 0, alpha = .5, color = "darkgrey") +
+  geom_boxplot(outlier.colour = NA, fill = NA) +
+  labs(x = "Books", y = "Number of Words per Verse")
+
+
+
+
 
 
--- Case_Study_07/analysis/case_study_7.R
+++ Case_Study_07/analysis/case_study_7.R
@@ -1,5 +1,95 @@
 library(tidyverse)
+library(stringi)
 scriptures <- rio::import("http://scriptures.nephi.org/downloads/lds-scriptures.csv.zip")
 
 # to get the Savior names
 bmnames <- read_rds(gzcon(url("https://byuistats.github.io/M335/data/BoM_SaviorNames.rds")))
+
+
+scriptures %>% 
+  filter(volume_title == "Book of Mormon") %>% 
+  group_by(book_short_title) %>% 
+  summarise(number = stri_stats_latex(scripture_text)[4])  
+
+
+
+
+
+
+scriptures %>% 
+  filter(volume_title == "Book of Mormon") %>%   
+  group_by(book_short_title) %>% 
+  summarise(text = str_c(scripture_text, collapse = " "),
+            count = str_count(text, "Jesus")) %>% 
+  select(book_short_title, count)
+
+
+
+
+book_verse <- scriptures %>% 
+  filter(volume_title == "Book of Mormon") %>% 
+  group_by(book_title) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+###### for loop examples #######
+verse <- scriptures %>% 
+  filter(volume_title == "Book of Mormon") %>% 
+  group_by(book_title) %>% .$scripture_text %>% str_flatten()
+
+bmn <- bmnames$name %>% rev()
+  
+for (i in seq_along(bmn)){
+  
+   parts <- str_replace_all(verse, bmn[i], "CALLANMIX") %>% unlist()
+   
+}
+
+split_text <- str_split(parts, "CALLANMIX") %>% unlist()
+
+answer <- data.frame(text = split_text) %>% 
+  mutate(words = unlist(map(text, function(x)(unname(stri_stats_latex(x)[4])))))
+
+answer %>% 
+  summarise(mean = mean(words))
+
+
+
+
+
+
+
+
+
+
+##################################################################
+
+#### Count of Names of God in the Book of Mormon
+
+store=c()
+for (i in seq_along(bmn)){
+  store[i] <- str_count(verse,bmn[i])
+}
+
+dat <- data.frame(name = bmn, count = store) %>% 
+  arrange(desc(count)) %>% filter(count >= 30)
+
+dat_name <- dat %>%  arrange(desc(count)) %>% filter(count >= 30) %>% .$name %>%  unique()
+
+dat %>% 
+  ggplot(aes(x = factor(name, levels = dat_name), y = count)) +
+  geom_point() +theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 0.5)) +
+  labs(title = "Top 20 Used Names of God in the Book of Mormon",
+       x = "Names of God", y = "Count of Times Name Used")
+
+
